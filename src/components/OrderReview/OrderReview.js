d50613fcc48d7483e71dae0659ac86444580baa1
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-01 16:59:23 +0600

    added remove item

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -2,14 +2,24 @@ import React from "react";
 import Cart from "../Cart/Cart";
 import useCart from "../hooks/useCart";
 import useProducts from "../hooks/useProducts";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const OrderReview = () => {
   const [products] = useProducts();
-  const [cart] = useCart(products);
+  const [cart, setCart] = useCart(products);
+
+  //   add even handler
+  const handleRemove = (key) => {
+    //   remove item logic here
+    const newCart = cart.filter((product) => product.key !== key);
+    setCart(newCart);
+  };
   return (
     <div className="row w-75 mx-auto">
       <div className="col-md-9">
-          
+        {cart.map((product) => (
+          <ReviewItem product={product} key={product.key} handleRemove={handleRemove}></ReviewItem>
+        ))}
       </div>
       <div className="col-md-3 border-start mt-3">
         <Cart cart={cart}></Cart>
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEraser } from "@fortawesome/free-solid-svg-icons";
+
+const ReviewItem = (props) => {
+  const { name, price, quantity, key } = props.product;
+  const { handleRemove } = props;
+  return (
+    <div className="mt-3 border-bottom">
+      <h4 className="text-primary"> {name} </h4>
+      <p className="lead">Price: {price} </p>
+      <p className="lead">Quantity: {quantity} </p>
+      <button onClick={() => handleRemove(key)} className="btn btn-warning px-3 fs-5 mb-2">
+        {" "}
+        <FontAwesomeIcon icon={faEraser} /> Remove
+      </button>
+    </div>
+  );
+};
+
+export default ReviewItem;
