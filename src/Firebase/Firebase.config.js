46e1ac16649aae4b2ca656af9304fc67ac910cfb
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-25 16:33:31 +0600

    added node mongo

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA3MtX9LjmwCgPDrCSaijK9DfJoR-o3G-Q",
-  authDomain: "ema-john-simple-auth-88e5a.firebaseapp.com",
-  projectId: "ema-john-simple-auth-88e5a",
-  storageBucket: "ema-john-simple-auth-88e5a.appspot.com",
-  messagingSenderId: "657801306948",
-  appId: "1:657801306948:web:1dad31ed6c775f9c302d11",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,40 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
-import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import { addToDb } from "../../utilities/fakedb";
 import "./Shop.css";
 import { NavLink } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useCart();
+  const [page, setPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
   // products to be rendered on the UI
   const [displayProducts, setDisplayProducts] = useState([]);
-
+  const size = 10;
   useEffect(() => {
-    fetch("./products.json")
+    fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
-        setDisplayProducts(data);
+        setProducts(data.products);
+        setDisplayProducts(data.products);
+        const count = data.count;
+        const pageNumber = Math.ceil(count / size);
+        setPageCount(pageNumber);
       });
-  }, []);
+  }, [page]);
 
-  useEffect(() => {
-    if (products.length) {
-      const savedCart = getStoredCart();
-      const storedCart = [];
-      for (const key in savedCart) {
-        const addedProduct = products.find((product) => product.key === key);
-        if (addedProduct) {
-          const quantity = savedCart[key];
-          addedProduct.quantity = quantity;
-          storedCart.push(addedProduct);
-        }
-      }
-      setCart(storedCart);
-    }
-  }, [products]);
+  //
 
   const handleAddToCart = (product) => {
     const exists = cart.find((pd) => pd.key === product.key);
@@ -47,8 +39,6 @@ const Shop = () => {
       product.quantity = 1;
       newCart = [...cart, product];
     }
-
-    // console.log(newCart);
     setCart(newCart);
     // save to local storage (for now)
     addToDb(product.key);
@@ -61,7 +51,6 @@ const Shop = () => {
 
     setDisplayProducts(matchedProducts);
   };
-
   return (
     <>
       <div className="bg-secondary py-2">
@@ -72,6 +61,14 @@ const Shop = () => {
           {displayProducts.map((product) => (
             <Product key={product.key} product={product} handleAddToCart={handleAddToCart}></Product>
           ))}
+          {/* pagination. conditional class uses */}
+          <div className="pagination py-5">
+            {[...Array(pageCount).keys()].map((number) => (
+              <button className={number === page ? "selected" : ""} key={number} onClick={() => setPage(number)}>
+                {number + 1}
+              </button>
+            ))}
+          </div>
         </div>
         <div className="col-md-3 border-start mt-3">
           <Cart cart={cart}>
